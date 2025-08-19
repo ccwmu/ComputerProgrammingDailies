bc85908e1c53d0febee73953ab67873a10343a06
Ethan Ye <147452670+ccwmu@users.noreply.github.com>
2025-08-19 14:19:33 -0700

    Add files via upload

---
+++ day1.py
@@ -0,0 +1,10 @@
+# imports randint function from random module
+from random import randint
+# constant list of elements
+FORTY_NINERS = ["abc", "xyz", "nmp", "agh", "diufh", "dahj", "dsf"]
+
+def rando_list_elem(array): # function declaration
+    index = randint(0, len(array) - 1) # determines random value, from the 1st index of list (0) to last index of list (len-1)
+    return array[index] # returns the element at the index
+
+print(rando_list_elem(FORTY_NINERS)) # print statement
